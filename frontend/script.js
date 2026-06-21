a685e9d944678844468246aa0f03b029150e6134
Temur Sadathanov <temursadathanov@MacBook-Pro-von-Temur.fritz.box>
2026-06-21 23:20:14 +0200

    fix api

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-fetch("https://429v86y6hl.execute-api.eu-north-1.amazonaws.com/count")
+fetch("https://429v86y6hl.execute-api.eu-north-1.amazonaws.com")
   .then(response => response.json())
   .then(data => {
     document.getElementById("visitor-count").textContent = data.count;
