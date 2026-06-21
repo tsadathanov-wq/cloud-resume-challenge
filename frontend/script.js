4a3cc3c1578c09a1f1e7acc5e864d245c830b8b4
Temur Sadathanov <temursadathanov@MacBook-Pro-von-Temur.fritz.box>
2026-06-21 23:16:05 +0200

    connect visitor counter to API

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,6 @@
-document.getElementById("counter").innerText = "0";
+fetch("https://429v86y6hl.execute-api.eu-north-1.amazonaws.com/count")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("visitor-count").textContent = data.count;
+  })
+  .catch(error => console.error("Error:", error));
