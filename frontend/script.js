6815184d4cb69fac31bf0f5886f95b0ca3a9fc24
Temur Sadathanov <temursadathanov@Mac.fritz.box>
2026-06-22 08:44:08 +0200

    fix visitor counter

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,9 @@
-fetch("https://429v86y6hl.execute-api.eu-north-1.amazonaws.com")
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("visitor-count").textContent = data.count;
-  })
-  .catch(error => console.error("Error:", error));
+fetch("https://429v86y6hl.execute-api.eu-north-1.amazonaws.com/count")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("counter").textContent = data.count;
+    })
+    .catch(error => {
+        console.error(error);
+        document.getElementById("counter").textContent = "Error";
+    });
